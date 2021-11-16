b0ebe08c61c032ab0a3f41141bb6eb283bca973b
Marion Saul <marionsaul.dev@gmail.com>
2021-11-16 15:22:05 +0100

    test redirect / to /testapp

--- routes/testApp.js
+++ routes/testApp.js
@@ -2,6 +2,7 @@ var express = require('express')
 var router = express.Router()
 const { testApp } = require('../controllers/testApp')
 
+// Go to this route on root
 router.get('/', testApp)
 
 module.exports = router
